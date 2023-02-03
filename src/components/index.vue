4c9813cb3bed8f9bca968bd5ac2227ed2da7d403
kamran08 <ahmedkamran265@gmail.com>
2023-02-03 13:42:23 +0000

    updated

---
+++ src/components/index.vue
@@ -0,0 +1,179 @@
+<template>
+    <div>
+        <div class="_1main_content">
+            <!-- Banner -->
+            <div class="_1banner">
+                <div class="container">
+                    <div class="col-12 col-md-8 col-lg-6">
+                        <h1 class="_1banner_title">LEGAL CASES</h1>
+                        <h2 class="_1banner_subtitle">With our recognized leadership in applying legal service and technology innovation, we deliver commercial advantage to clients.</h2>
+                        <p class="_1banner_text">Our lawyers offer clients a range of integrated global capabilities, including some of the world’s most active M&A, real estate, financial services, litigation and corporate risk practices.</p>
+                        <button class="_2btn _btn_large">Find Out More</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Banner -->
+
+            <!-- Services -->
+            <div class="_1home_services">
+                <div class="container">
+                    <h1 class="_home_title">Our services</h1>
+
+                    <div class="row">
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">CORPORATE & FINANCE | TAX</p>
+                                    <p class="_1card_text"> Not Confer Litigation Issues Arising from Transactions</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image2.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">REGULATORY & SPECIALTY TAX</p>
+                                    <p class="_1card_text">Litigation Issues Arising from Transactions</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image3.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">INTERNATIONAL</p>
+                                    <p class="_1card_text">Finance & Risk on Outlook for Project Development</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image2.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">INTERNATIONAL</p>
+                                    <p class="_1card_text">Finance & Risk on Outlook for Project Development</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image4.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">CORPORATE & FINANCE | TAX</p>
+                                    <p class="_1card_text"> Not Confer Litigation Issues Arising from Transactions</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                        <!-- Card -->
+                        <div class="col-12 col-md-4 col-lg-4">
+                            <router-link to="" class="_1card">
+                                <div class="_1card_img">
+                                    <img class="_1card_pic" src="../../static/img/image.jpg" alt="" title="">
+                                </div>
+
+                                <div class="_1card_bottom">
+                                    <p class="_1card_title">CORPORATE & FINANCE | TAX</p>
+                                    <p class="_1card_text"> Not Confer Litigation Issues Arising from Transactions</p>
+                                </div>
+                            </router-link>
+                        </div>
+                        <!-- Card -->
+                    </div>
+                </div>
+            </div>
+            <!-- Services -->
+
+            <!-- Contact -->
+            <div class="_home_contact">
+                <div class="_home_contact_left">
+                    <h1 class="_home_title">Free Case Evaluation</h1>
+                    <p class="_home_contact_text">Contacting the firm is free. We understand that the disputes facing you, your family or your business can seem daunting.</p>
+
+                    <div class="_home_contact_sub">
+                        <div class="_home_contact_input">
+                            <input placeholder="Name" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <input placeholder="Email" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <input placeholder="Subject" class="_home_contact_int">
+                        </div>
+                        <div class="_home_contact_input">
+                            <textarea :rows="4" class="_home_contact_int" placeholder="Message"></textarea>
+                        </div>
+
+                        <div class="_home_contact_button">
+                            <button class="_1btn _btn_large">Submit</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="_home_contact_right">
+                    <h1 class="_home_title">Contact us</h1>
+
+                    <p class="_home_contact_text">With our recognized leadership in applying legal service and technology innovation, we deliver commercial advantage to clients.</p>
+
+                    <div class="_home_contact_main">
+                        <p class="_home_contact_info">
+                            OFFICE
+                            <span class="_home_contact_info_text">1760 Wilshire Blvd., Suite 1, LA, CA 90025</span>
+                        </p>
+                        <p class="_home_contact_info">
+                            EMAIL
+                            <router-link to="" class="_home_contact_info_text">example@example.com </router-link>
+                        </p>
+                        <p class="_home_contact_info">
+                            TEL
+                            <router-link to="" class="_home_contact_info_text">+88 01790302229</router-link>
+                        </p>
+                    </div>
+
+                    <div class="_home_contact_socail">
+                        <ul class="_1menu_social">
+                            <li>
+                                <a href=""><Icon type="logo-facebook" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-linkedin" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-twitter" /></a>
+                            </li>
+                            <li>
+                                <a href=""><Icon type="logo-youtube" /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- Contact -->
+        </div>
+    </div>
+</template>
